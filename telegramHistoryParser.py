a41748a6b460f3b3e73c0edaf14a0ac794933b48
Rayan Gosling <104678305+realpalyanytsya@users.noreply.github.com>
2022-05-19 12:24:30 +0300

    Add files via upload

---
+++ telegramHistoryParser.py
@@ -0,0 +1,55 @@
+import bs4
+import re
+import os
+
+words = []
+
+def findFiles():
+    messages = []
+    regex = re.compile('^messages[1-9|0-9]*.html$')
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if regex.match(file):
+                messages.append(file)
+
+    print('Finded {} files with messages'.format(len(messages)))
+    return messages
+
+
+def scanFiles(messages):
+    tmp = []
+
+    for file in messages:
+
+        with open(file, 'r') as f:
+            soup = bs4.BeautifulSoup(f, "html.parser")
+            text = soup.find_all('div', class_='text')
+
+            print('+ {} sanned'.format(file))
+
+            for txt in text:
+                tmp.append(txt.text.split())
+
+            for i in tmp:
+                for j in i:
+                    j = re.findall('[а-яґєії|[А-ЯҐЄІЇ]|a-z|A-Z]+', j)
+
+                    words.append(''.join(map(str, j)).lower())
+
+            tmp.clear()
+        f.close()
+
+
+def writeInFile(fileName):
+    with open(fileName, 'w') as f:
+        for i in words:
+            f.write(i + ' ')
+
+    print('Result file is: {}'.format(fileName))
+
+
+if __name__ == '__main__':
+    files = findFiles()
+
+    scanFiles(files)
+    writeInFile('words.txt')
